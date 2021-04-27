4630a2e34939aa70a3a90ab929e36cbceb331805
MichaelHabermas <michaelghabermas@gmail.com>
2021-04-27 16:36:16 -0400

    LI SU

---
+++ recipies/src/components/SignUpForm.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SignUpFormDiv = styled.div`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	.container {
+		height: auto;
+		width: 50%;
+		box-shadow: 0px 5px 10px RGBA(225, 226, 224, 1);
+		background: linear-gradient(to right, #ccbe1d, #fc9471);
+		margin: 0 auto;
+		border-radius: 40px;
+	}
+
+	h1 {
+		color: white;
+		text-align: center;
+		padding: 2%;
+	}
+
+	form {
+		display: flex;
+
+		justify-content: center;
+		align-items: center;
+	}
+	ul {
+		list-style-type: none;
+		margin: 0 auto 2rem;
+	}
+
+	li {
+		display: flex;
+		flex-direction: column;
+
+		/* flex-flow: column wrap; */
+		justify-content: center;
+		align-items: center;
+		/* padding: 1rem; */
+		margin: 0.5rem 0 1rem 0;
+	}
+
+	input {
+		border-radius: 25px;
+		border: none;
+		width: 500px;
+		height: 50px;
+	}
+
+	.buttons {
+		margin: 0% auto;
+		padding: 2% 0;
+		display: flex;
+		flex-flow: horizontal wrap;
+		justify-content: space-between;
+		width: 40%;
+	}
+`;
+
+export default function SignUpForm({ logInInfo, setLogInInfo }) {
+	// const change = ev => {
+	// 	const { name, value } = ev.target;
+	// 	console.log(name);
+	// 	console.log(value);
+	// 	setFormValues({ ...formValues, [name]: value });
+	// };
+
+	// const submit = ev => {
+	// 	ev.preventDefault();
+	// 	const newRecipe = {
+	// 		source: formValues.source,
+	// 		email: formValues.email,
+	// 		recipeName: formValues.recipeName,
+	// 		ingrediants: formValues.ingrediants,
+	// 		instructions: formValues.instructions
+	// 	};
+	// 	axios
+	// 		.post('https://buildweekrecipes.herokuapp.com/api/recipes', newRecipe)
+	// 		.then(res => {
+	// 			setFormValues(initialValues);
+	// 			console.log(initialValues);
+	// 		})
+	// 		.catch(err => {});
+	// };
+
+	return (
+		<SignUpFormDiv>
+			<div className="container">
+				<h1>Create a ReciPeazy IDeazy</h1>
+				<form>
+					<ul>
+						<li>
+							User Name:
+							<input type="text" onChange={() => console.log('username changing')} name="username" />
+						</li>
+						<li>
+							Password:
+							<input type="password" onChange={() => console.log('password changing')} name="password" />
+						</li>
+					</ul>
+				</form>
+			</div>
+		</SignUpFormDiv>
+	);
+}
